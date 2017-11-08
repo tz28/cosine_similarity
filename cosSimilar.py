811e3620fadd7e3fe49c7e2c33b4db572e97f2df
hc <389096133@qq.com>
2017-11-08 10:33:33 +0800

    Update cosSimilar.py

--- cosSimilar.py
+++ cosSimilar.py
@@ -2,7 +2,7 @@ import numpy as np
 def cosine_similarity(a,b): 
     a=np.mat(a)#把list转换为matrix
     b=np.mat(b)
-    num=float(a.T * b)
+    num=float(a * b.T)
     #linalg.norm()计算向量的范数
     denom=np.linalg.norm(a)*np.linalg.norm(a)
     return 0.5+0.5*(num/denom)#cosine similarity的范围为[-1,1],先把他归一化至[0,1]
